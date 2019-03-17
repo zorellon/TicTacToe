1ba2300cacda9601b4f7ffff82003979db12a1a2
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-17 19:08:37 -0400

    implementing play turn function

--- src/TicTacToeLogic.java
+++ src/TicTacToeLogic.java
@@ -13,6 +13,24 @@ public class TicTacToeLogic {
 		this.userMarker = playerToken;
 		this.aiMarker = aiMarker;
 		this.winner = '-';
-	`this.board = setBoard();
+	    this.board = setBoard();
+	}
+	
+	public static char[] setBoard() {
+		char[] board = new char[9];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = '-';
+		}
+		return board;
+	}
+	
+	public boolean playTurn(int spot) {
+		boolean isValid = withinRange(spot) && !isSpotTaken(spot);
+		if(isValid ) {
+			board[spot-1] = currentMarker;
+			currentMarker = (currentMarker == userMarker) ? aiMarker : userMarker;
+			
+		}
+		return isValid;
 	}
 }
