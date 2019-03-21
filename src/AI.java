05ea4563f573aa903ef83ecf569bd5ca2c34918e
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-21 09:58:58 -0400

    implementing ai class

--- src/AI.java
+++ src/AI.java
@@ -1,5 +1,19 @@
 
-public class AI {
 
+import java.util.ArrayList;
+import java.util.Random;
+
+public class AI {
+	public int pickSpot(TicTacToeLogic game) {
+		ArrayList<Integer> choices = new ArrayList();
+		for (int i = 0; i<9; i++) {
+			if (game.board[i] == '-') {
+				choices.add(i+1);
+			}
+		}
+		Random rand = new Random();
+		int choice = choices.get(Math.abs(rand.nextInt() % choices.size()));
+		return choice;
+	}
 	
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
 
 public class TicTacToe {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		boolean doYouWannaPlay = true;
+		while (doYouWannaPlay) {
+			System.out.println(" Welcome to Tic Tac Toe!"
+					+"Are you ready to face the Ai?");
+			System.out.println();
+			System.out.println("Choose If you would like to be X's or O's by entering it now.");
+			char playerToken = scan.next().charAt(0);
+			char aiToken;
+			if (playerToken == 'o' || playerToken == 'O' ) {
+				aiToken = 'X';
+			}else if (playerToken == 'x' || playerToken == 'X' ) {
+				aiToken = 'O';
+			}else {
+				aiToken = 'X';
+			}
+			
+			TicTacToeLogic game = new TicTacToeLogic(playerToken,aiToken);
+			AI ai = new AI();
+			
+			//Setup Game
+			System.out.println();
+			System.out.println("Enter a index where you would like to put your token.");
+			TicTacToeLogic.printIndexBoard();
+			System.out.println();
+			
+			
+			
+		}
+		
 
 	}
 
